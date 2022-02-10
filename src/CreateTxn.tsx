ab8ba518386980b642523c5bda16971b4956f97e
ebrahimiana <ebrahimian.a@husky.neu.edu>
2022-02-09 21:22:25 -0500

    start building create

--- src/CreateTxn.tsx
+++ src/CreateTxn.tsx
@@ -1,19 +1,55 @@
-import { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import RadioGroup from '@mui/material/RadioGroup';
+import { useState } from "react";
+import {
+    TextField,
+    RadioGroup,
+    FormControlLabel,
+    Radio,
+    InputAdornment,
+} from "@mui/material";
 
 import "./CreateTxn.scss";
 
 function CreateTxn() {
-  return (
-    <div className="create-main">
-      <h1>Create a new transaction here.</h1>
-      <form className="create-form">
-          <TextField variant="standard" label='Amount' />
-          <TextField variant="standard" label='Location' />
-      </form>
-    </div>
-  );
+    const [amountError, setAmountError] = useState(false);
+    const [amount, setAmount] = useState(0);
+
+    const amountErrorMessage = "Amount entered must be a number.";
+
+    return (
+        <div className="create-main">
+            <h1>Create a new transaction here.</h1>
+            <form className="create-form">
+                <TextField
+                    variant="standard"
+                    label="Amount"
+                    error={amountError}
+                    helperText={amountError && amountErrorMessage}
+                    onChange={(e) => {
+                        const convertedAmount = Number(e.target.value);
+                        if (isNaN(convertedAmount)) {
+                            setAmountError(true);
+                        } else {
+                            setAmount(convertedAmount);
+                            setAmountError(false);
+                        }
+                    }}
+                />
+                <TextField variant="standard" label="Location" />
+                <RadioGroup row defaultValue="spent">
+                    <FormControlLabel
+                        value="spent"
+                        label="Spent"
+                        control={<Radio />}
+                    />
+                    <FormControlLabel
+                        value="received"
+                        label="Received"
+                        control={<Radio />}
+                    />
+                </RadioGroup>
+            </form>
+        </div>
+    );
 }
 
 export default CreateTxn;
